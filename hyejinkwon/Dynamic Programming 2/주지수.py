85ad53e7126b537ee87861a35c4faad10e563fad
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-04-21 15:27:32 +0900

    [PGS] 예상 대진표 / Lv2

--- hyejinkwon/Dynamic Programming 2/주지수.py
+++ hyejinkwon/Dynamic Programming 2/주지수.py
@@ -13,8 +13,6 @@ for i in range(1,N+1) :
     for j in range(1,M+1) :
         dp[i][j] = town[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
 
-print(dp)
-
 K = int(input())
 for _ in range(K) :
     x1,y1,x2,y2 = map(int, input().split())

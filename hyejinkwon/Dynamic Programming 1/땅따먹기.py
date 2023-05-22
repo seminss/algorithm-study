4a1b4b90047b2f18c1933811438f236ce708cd16
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-05-22 11:07:04 +0900

    [PGS] 방문길이 / Lv2

--- hyejinkwon/Dynamic Programming 1/땅따먹기.py
+++ hyejinkwon/Dynamic Programming 1/땅따먹기.py
@@ -5,9 +5,9 @@ def solution(land):
     answer = 0
     
     for i in range(1, len(land)) :
-        for j in range(4) :
+        for j in range(4) : 
             land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
-
+    
     answer = max(land[len(land)-1])
             
     return answer

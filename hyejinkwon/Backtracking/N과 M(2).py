8c926872cdc5e7e1a8d9d877eabcb8b25aead2dc
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-09-10 21:42:02 +0900

    [BOJ] N과 M(2) / Sliver3

---
+++ hyejinkwon/Backtracking/N과 M(2).py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N,M = list(map(int,input().split()))
+s = []
+def dfs(start):
+    if len(s)==M:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(start,N+1):
+        if i not in s:
+            s.append(i)
+            dfs(i+1)
+            s.pop()
+dfs(1)

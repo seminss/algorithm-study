bf6cd9712ffde115e9105582773e65cc64ac58ac
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-09-17 15:45:55 +0900

    [BOJ] 덩치 / Silver5

---
+++ hyejinkwon/solvedAC/덩치.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+human = []
+
+for _ in range(N) :
+    x,y = map(int,input().split())
+    human.append([x,y])
+
+for i in range(N) :
+    rank = N
+    for j in range(N) :
+        if i!=j and human[i][0] > human[j][0] and human[i][1] > human[j][1] :
+            rank -= 1
+    print(rank, end=" ") 

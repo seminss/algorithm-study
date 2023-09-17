5fdf3ea4e17e50be3f4a783903b8d468bf5942ac
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-09-17 20:05:49 +0900

    [BOJ] 피보나치 함수 / Sliver3

--- hyejinkwon/solvedAC/마인크래프트.py
+++ hyejinkwon/solvedAC/마인크래프트.py
@@ -1,26 +1,28 @@
 import sys
 
 input = sys.stdin.readline
-N,M,B = map(int,input().split())
-ground = []
+
+N,M,B = map(int, input().split())
+ground = [list(map(int, input().split())) for _ in range(N)]
 floor = 0
-time = int(1e9)
+answer = int(1e9)
 
-ground = [list(map(int, input.split())) for _ in range(N)]
+for i in range(257):
+    check1, check2 = 0,0
 
-for i in range(257) :
-    check_1 = 0
-    check_2 = 0
-    for x in range(N) :
-        for y in range(M) :
-            if ground[x][y] > i :
-                check_2 += ground[x][y] - i
-            else :
-                check_1 += i - ground[x][y]
-
-    if check_1 <= check_2 + B :
-        
-        if check_2*2 + check_1 <= int(1e9) :
-            time = check_2*2 + check_1
-            floor = i
-print(time, floor)
+    for x in range(N):
+        for y in range(M):
+            if ground[x][y] < i:
+                check1 += (i - ground[x][y])
+            else:
+                check2 += (ground[x][y] - i)
+    
+    inventory = check2 + B
+    if inventory < check1:
+        continue
+    
+    time = 2 * check2 + check1
+    if time <= answer:
+        answer = time
+        floor = i
+print(answer, floor)

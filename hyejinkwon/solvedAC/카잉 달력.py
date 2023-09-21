53d8cc23155fc678e8881ed20733471428683e25
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-09-21 22:53:26 +0900

    [BOJ] 카잉 달력 / Silver1

--- hyejinkwon/solvedAC/카잉 달력.py
+++ hyejinkwon/solvedAC/카잉 달력.py
@@ -3,9 +3,8 @@ import sys
 input = sys.stdin.readline
 T = int(input())
 for _ in range(T) :
-    N,M,X,Y = map(int, input().split())
+    M,N,X,Y = map(int, input().split())
 
-    answer = False
     for K in range(X, M*N+1,M) :
         if (K-X)%M ==0 and (K-Y)%N==0 : 
             print(K)

e222a5b1e3bab2385a85778fb55847d7ef3f1ca0
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-09-22 21:32:00 +0900

    [BOJ] 2xn 타일링 / Silver3

--- hyejinkwon/Dynamic Programming 1/2xn 타일링.py
+++ hyejinkwon/Dynamic Programming 1/2xn 타일링.py
@@ -1,28 +1,26 @@
-# 가로 2 세로 1을 이용해 세로 2 가로 n인 바닥 채우기 !
-# 가로 n을 채우는 게 관건 : 2와 1을 이용해 합이 n을 만들 수 있느냐 ?
-# DP 이용 !
+import sys
 
-# n = 1 -> 1         : 1
-# n = 2 -> 11 2      : 2
-# n = 3 -> 111 21 12 : 3
-# n = 4 -> 1111 22 121 112 211 : 5
-# dp[i] = dp[i-1] +dp[i-2]
+input = sys.stdin.readline
+n = int(input())
 
-def solution(n):
-    answer = 0
-    dp = [0] * n
-    
-    if n == 1:
-        return 1
-    if n == 2 :
-        return 2
+# n = 1 -> 1가지
+# n = 2 -> 2가지
+# n = 3 -> 3가지
+# n = 4 -> 5가지
 
-    if n >= 3 :
-        dp[0], dp[1] = 1,2
-        
-        for i in range(2,n) :
-            dp[i] = (dp[i-1] + dp[i-2])%1000000007
-    
-        answer = dp[-1]
-        
-        return answer
+dp = [0]*(n)
+
+if n == 1 : 
+    dp[n-1] = 1 
+    print(dp[n-1]%10007)
+elif n == 2 : 
+    dp[n-1] = 2
+    print(dp[n-1]%10007)
+else :
+    dp[0] = 1 
+    dp[1] = 2
+
+    for i in range(2,n) :
+        dp[i] = dp[i-1] + dp[i-2]
+
+    print(dp[n-1]%10007)

51fc25c66070b54f4fcc54d73e16ea14f1b40818
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-10-21 14:41:47 +0900

    [PGS] 타겟 넘버 / 고득점kit

--- hyejinkwon/DFS, BFS/타겟 넘버.py
+++ hyejinkwon/DFS, BFS/타겟 넘버.py
@@ -4,19 +4,17 @@ from collections import deque
 def solution(numbers, target):
     answer = 0
     queue = deque()
-    
-    # index가 중요
     queue.append([numbers[0], 0])
-    queue.append([-1*numbers[0], 0])
+    queue.append([numbers[0]*-1, 0])
     
     while queue :
-        value, index = queue.popleft()
-        index += 1
-        if index < len(numbers) :
-            queue.append([value+numbers[index], index])
-            queue.append([value-numbers[index], index])
-        else : # numbers 리스트 끝까지 다 돌았을 때
-            if value == target :
-                answer += 1
+        val, i = queue.popleft()
+        i += 1
         
+        if i < len(numbers) :
+            queue.append([val + numbers[i], i])
+            queue.append([val - numbers[i], i])
+        else :
+            if val == target : answer += 1
+    
     return answer
